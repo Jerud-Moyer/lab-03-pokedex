5c67b97937726054ac2ca7fa12b7aef29ab3a5b2
Jerud-Moyer <jerudmoyer@gmail.com>
2020-08-07 08:36:51 -0700

    hi

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import {
     BrowserRouter as Router, 
     Route, 
     Switch,
-    Link,
+    
 } from 'react-router-dom';
-import PokeSearch from './Search/PokeSearch.js';
+
 import SearchPage from './SearchPage/SearchPage.js';
 import DetailPage from './DetailPage/DetailPage.js';
-import request from 'superagent';
+
 
 export default class App extends Component {
     state = {
